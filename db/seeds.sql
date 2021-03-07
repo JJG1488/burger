39ec7dfcf05c4ec34e380dc1afca489f65262309
James <jamesgault1488@gmail.com>
2021-03-07 16:04:16 -0500

    created db folder/ created schema and seed files inside of db folder

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('classic burger', false);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ('bacon burger', false);
+INSERT INTO burgers(burger_name, devoured)
+VALUES ('veggie buger', false);
